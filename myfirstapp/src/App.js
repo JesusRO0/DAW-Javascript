5bba99851d7edb2381d2fa6ccfd92516acd2e4f0
JesusRO0 <6193@iespuertasdelcampo.es>
2022-03-11 20:21:34 +0100

    Fin de State con Reactjs

--- myfirstapp/src/App.js
+++ myfirstapp/src/App.js
@@ -1,7 +1,9 @@
+//Siempre hay que importar la biblioteca React.
 import React from 'react';
 import './App.css';
 
-/*function Helloworld(props){
+/* Esto hace lo mismo que la clase pero en función.
+function Helloworld(props){
   return (
   <div id="hello"><h3>{props.subtitulo}</h3>{props.texto}</div>
   );
@@ -9,26 +11,32 @@ import './App.css';
 
 class Helloworld extends React.Component {
 
+  //Proporcionar un estado, este es un estado de ejemplo donde se podra visualizar o no, un contenido.
   state = {
     show: true
   }
-
+  //Esta función cambia el estado de true a false y viceversa.
+  Mostrar =() => {
+    this.setState({show: !this.state.show})
+  }
+  //La función render se utiliza en las clases para devolver algo, en este caso el div o el h3.
   render(){
-    if(this.state.show = true){
+    //Si el show es true muestra el div y si es false el h3, esto cambiará pulsando el botón.
+    if(this.state.show){
+      //Dentro de los botónes colocaremos la función mostrar.
     return (
       <div id="hello">
         <h3>{this.props.subtitulo}</h3>
         {this.props.texto}
-        <button>Visualizar</button>
+        <button onClick={this.Mostrar}>Visualizar</button>
       </div>
-      
-      )
+        )
     }else{
-      return <h1>No hay elementos.</h1>
+      return <h1>No hay elementos.<button onClick={this.Mostrar}>Visualizar</button></h1>
     }
   }
 }
-
+//Función de nuestra app donde retornaremos lo que hará.
 function App() {
   return (
     <div>Esto es mi componente: 
