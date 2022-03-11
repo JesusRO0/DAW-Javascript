ac9aab4784a90093040595807dd481804eedde86
JesusRO0 <6193@iespuertasdelcampo.es>
2022-03-11 19:57:27 +0100

    Cambios en la Aplicación de React

--- myfirstapp/src/App.js
+++ myfirstapp/src/App.js
@@ -1,10 +1,32 @@
 import React from 'react';
 import './App.css';
 
-function Helloworld(props){
+/*function Helloworld(props){
   return (
   <div id="hello"><h3>{props.subtitulo}</h3>{props.texto}</div>
   );
+}*/
+
+class Helloworld extends React.Component {
+
+  state = {
+    show: true
+  }
+
+  render(){
+    if(this.state.show = true){
+    return (
+      <div id="hello">
+        <h3>{this.props.subtitulo}</h3>
+        {this.props.texto}
+        <button>Visualizar</button>
+      </div>
+      
+      )
+    }else{
+      return <h1>No hay elementos.</h1>
+    }
+  }
 }
 
 function App() {
